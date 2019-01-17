c3c8a5af6d3f4ce0d92b750abfa72d9bb99104e8
Claudio Orellana <claudio.ore94@gmail.com>
2019-01-17 03:08:14 -0300

    pruebo uniendo la ruta con path.join

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -97,9 +97,9 @@ app.post('/create', authentication, async (req, res)=> {
     let nombreImagen = `${nombreCortado.join('.')}-${new Date().getMilliseconds()}.${extension}`
 
     //muevo la foto a carpeta 
-    await image.mv(`public/uploads/${nombreImagen}`, (error) => {
+    await image.mv(path.join('public','uploads',nombreImagen), (error) => {
         if(error){
-            return res.status(500).json({
+            return res.status(400).json({
                 ok: false,
                 error
             });
