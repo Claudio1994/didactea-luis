5567c6ca08a4c21971cd31dbb0a703d50b5ba9a0
Claudio Orellana <claudio.ore94@gmail.com>
2019-01-17 02:23:09 -0300

    agrego la ultima version al build

---
+++ client/build/precache-manifest.f874d2b01cbf4c2a4b89127f1ad67ca0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39ed4443aaeb150b7da1",
+    "url": "/static/js/main.39ed4443.chunk.js"
+  },
+  {
+    "revision": "6eb7f9cf2dd6f901d6d7",
+    "url": "/static/js/1.6eb7f9cf.chunk.js"
+  },
+  {
+    "revision": "39ed4443aaeb150b7da1",
+    "url": "/static/css/main.79cee2e3.chunk.css"
+  },
+  {
+    "revision": "ced73b461fd0ab8b6b7af0cd3c0dcfe4",
+    "url": "/index.html"
+  }
+];
