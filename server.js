5de8de223f3133f22bf8d5cddd0d0b1b44cd6791
Claudio Orellana <claudio.ore94@gmail.com>
2019-01-21 03:53:47 -0300

    agrego la ultima version del build

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use(morgan('dev'));
 app.use('/api',require('./server/routes/index.routes'));
 
 // Static files
-app.use('/', express.static(path.join(__dirname+'/client/build')));
+app.use('/', express.static(path.join(__dirname,'client/build')));
 
 // Rutas con react-router que no coincidan con las rutas del backend
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname,'client/build/index.html'));
 });
 
 // Starting server
