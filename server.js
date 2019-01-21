81bc010672fa8defec695f0948fe83728f0f3472
Claudio Orellana <claudio.ore94@gmail.com>
2019-01-21 03:49:38 -0300

    quito el map del build

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.set( 'port', process.env.PORT || 5000 );
 app.use(morgan('dev'));
 //app.use(express.json());
 
-// Static files
-app.use('/public', express.static(path.join(__dirname+'/client/build')));
-
 // Routes
 app.use('/api',require('./server/routes/index.routes'));
 
+// Static files
+app.use('/', express.static(path.join(__dirname+'/client/build')));
+
 // Rutas con react-router que no coincidan con las rutas del backend
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/public/index.html'));
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 // Starting server
